f55cb0611b5acc3e24426e9456ca6c85b97b449d
ilias-h <ilias.hendo@gmail.com>
2014-10-21 09:35:35 +0200

    Create look.c

---
+++ DiskAlgorithms/look.c
@@ -0,0 +1,47 @@
+#include<limits.h>
+#include<stdio.h>
+#include"look.h"
+
+int LOOK(int parked, int vector[],int size){
+	int i, ready=0,now=parked,diff=INT_MAX,j,sum=0,checked[100]={0};
+
+	while(!ready){
+		for(i=0;i<size;i++){
+			if(vector[i]<=now && abs(vector[i]-now)<diff && checked[i]==0){
+				diff=abs(vector[i]-now);
+				j=i;
+			}
+
+		}
+
+		if(checked[j]==1){
+			ready = 1;
+			diff=INT_MAX;
+		}else{
+			sum += diff;
+			now = vector[j];
+			diff=INT_MAX;
+			checked[j]=1;
+		}
+
+
+	}
+	while(ready){
+		for(i=0;i<size;i++){
+			if(vector[i]>now && checked[i]==0 && abs(vector[i]-now)<diff){
+				diff=abs(vector[i]-now);
+				j=i;
+			}
+		}
+
+		if(checked[j]==1){
+			ready = 0;
+		}else{
+			sum += diff;
+			now = vector[j];
+			diff=INT_MAX;
+			checked[j]=1;
+		}	
+	}
+	return sum;
+}
